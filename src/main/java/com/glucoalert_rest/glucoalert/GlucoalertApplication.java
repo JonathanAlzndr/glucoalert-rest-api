4d79d587204388658dc5e79b267a7278aaec4069
Jonathan K <jkamagi41@gmail.com>
2024-12-02 22:48:54 +0800

    first commit

---
+++ src/main/java/com/glucoalert_rest/glucoalert/GlucoalertApplication.java
@@ -0,0 +1,13 @@
+package com.glucoalert_rest.glucoalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucoalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlucoalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glucoalert_rest/glucoalert/GlucoalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glucoalert_rest.glucoalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucoalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
